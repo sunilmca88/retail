b0f8ef60594fec828ed9421e386dbea94ea2c9e7
sunilmca88 <sunilmca88@gmail.com>
2020-09-14 16:50:19 +0530

    Modality-3 Integrated

--- assets/js/custom.js
+++ assets/js/custom.js
@@ -66,15 +66,24 @@ $(document).ready(function () {
         for (var i = 1; i <= $('option:selected', this).text(); i++) {
             var borrowerElem = '<div class="jumbotron"><div ><h5 class="alert alert-dark text-center" role="alert">Applicant #'+ i + 
             ' Details</h5></div><div class="row"><div class="col-sm-3"><label for="borrowerType-'+ i + '">Borrower Type</label>\
-                <select id="borrowerType-'+ i + '" class="form-control" '+isFRR+'><option selected value="">Select</option><option value="sal">Salaried</option>\
+                <select id="borrowerType-'+ i + '" class="form-control" '+isFRR+'><option selected value="" disabled>Select</option><option value="sal">Salaried</option>\
                   <option value="oth">Other Individual</option></select></div><div class="col-sm-3"><label id="lblIncomeLatest-'+i+'" for="latestInc-'+ i + 
                   '">'+incomeSrc[0]+'</label><input type="tel" class="form-control" id="latestInc-'+ i + 
                   '"  placeholder="Enter Value"></div><div class="col-sm-3"><label id="lblIncomeFeb20-'+i+'" for="feb20Inc-'+ i + 
                   '">'+incomeSrc[1]+'</label><input type="tel" class="form-control" id="feb20Inc-'+ i + 
                   '" placeholder="Enter Value"></div><div class="col-sm-3"><label for="totalDeduction-'+ i + 
                   '">Total Deduction</label><input type="tel" class="form-control" id="totalDeduction-'+ i + 
-                  '" placeholder="Enter Value" '+isFRR+'></div></div><br/><div class="row"><div class="col-sm-8"><label for="borrowerName-'+ i + 
-                  '">Customer Name</label><input type="text" class="form-control" id="borrowerName-'+ i + 
+                  '" placeholder="Enter Value" '+isFRR+'></div></div><br/>\
+                  <div class="row"><div class="col-sm-4"><label for="foir-'+i+
+                  '" data-toggle="tooltip" title="FOIR applicable as per present scheme guidelines in consonance with income level">\
+                      FOIR <sup><span class="badge badge-warning">i</span></sup></label>\
+                    <input type="tel" class="form-control" id="foir-'+i+'" placeholder="Enter Value"'+isFRR+'>\
+                  </div><div class="col-sm-4"><label for="1920Profit-'+i+'">100% Net Profit of 2019-20 </label>\
+                    <input type="tel" class="form-control" id="1920Profit-'+i+'" placeholder="Enter Value"'+isFRR+'>\
+                  </div><div class="col-sm-4"><label for="1819Profit-'+i+'" data-toggle="tooltip" title="If net profit of FY 2019-20 not available">\
+                      100% Net Profit of FY 2018-19 <sup><span class="badge badge-warning">i</span></sup></label>\
+                    <input type="tel" class="form-control" id="1819Profit-'+i+'" placeholder="Enter Value"'+isFRR+'></div></div><br/>\
+                  <div class="row"><div class="col-sm-8"><label for="borrowerName-'+ i + '">Customer Name</label><input type="text" class="form-control" id="borrowerName-'+ i + 
                   '" placeholder="Enter Name"></div><div class="col-sm-4"style="text-align:center;"><label>Percentage Reduction in Salary</label><br/>\
                     <h3 class="badge badge-danger" style="font-size: x-large;" id="borrowerImpact-'+ i + 
                 '"></h3></div></div></div>';
@@ -106,9 +115,13 @@ $(document).ready(function () {
         if(selectedBorrowerType === "sal"){
             $('#lblIncomeLatest-'+elemIndex).text("Latest Salary");
             $('#lblIncomeFeb20-'+elemIndex).text("Salary in Feb 2020");
+            $('#1920Profit-'+elemIndex).val("").attr('disabled', true);
+            $('#1819Profit-'+elemIndex).val("").attr('disabled', true);
         }else  if(selectedBorrowerType === "oth"){
             $('#lblIncomeLatest-'+elemIndex).text("Latest GMBR");
             $('#lblIncomeFeb20-'+elemIndex).text("Previous GMBR");
+            $('#1920Profit-'+elemIndex).val("").removeAttr('disabled')
+            $('#1819Profit-'+elemIndex).val("").removeAttr('disabled')
         }else{
             $('#lblIncomeLatest-'+elemIndex).text("");
             $('#lblIncomeFeb20-'+elemIndex).text("");
