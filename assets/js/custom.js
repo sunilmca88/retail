64dff08fb0a3e8c62e94d0817eaf2502b9eb4d1f
sunilmca88 <sunilmca88@gmail.com>
2020-09-17 16:29:47 +0530

    Updated modal

--- assets/js/custom.js
+++ assets/js/custom.js
@@ -62,10 +62,7 @@ $(document).ready(function () {
 
     /******Default function Initialisation starts here******/
     $('[data-toggle="tooltip"]').tooltip(); //Initializing  tooltip
-    $('#staticBackdrop').modal({
-        keyboard: false,
-        backdrop: 'static'
-      });
+    $('#staticBackdrop').modal();
     /******Default function Initialisation ends here******/
 
     $accType.change(function () {        
